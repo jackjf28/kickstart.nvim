d5185302e0562515ee932f43d117401c2b17bbaf
Jack Farrell <jackfarrell3030@gmail.com>
2023-09-10 15:05:26 -0500

    fix merge conflict

--- init.lua
+++ init.lua
@@ -358,11 +358,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 require('nvim-treesitter.install').compilers = { "clang", "gcc" }
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-<<<<<<< HEAD
-  ensure_installed = { 'c', 'cpp', 'c_sharp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
-=======
   ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },
->>>>>>> c80a77488bcfa73c69e5866a7529398609f5a2d3
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
